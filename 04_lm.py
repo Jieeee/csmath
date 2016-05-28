753254e8886ad2dc5728d8472f01ac1aafdd515a
Jieeee <993027639@qq.com>
2016-05-28 21:10:54 +0800

    Update 04_lm.py

--- 04_lm.py
+++ 04_lm.py
@@ -1,1 +1,6 @@
 # This code implements LM algorithm
+
+# I'm sorry for that my code doesn't work now. I have debugged it for several hours and
+# it make me exhausted. I don't want to copy it from others, so this code is left empty.
+
+# Please forgive!
